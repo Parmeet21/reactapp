009d610961f3442cbc1390e5f85c167b1f83da6d
parmeet21 <parmeetkundra21@gmail.com>
2023-03-10 15:27:53 +0530

    new

--- src/mycom/Footer.js
+++ src/mycom/Footer.js
@@ -4,6 +4,11 @@ export default function Footer() {
   return (
     <div className='container py-4 mt-4 border-top  footer'>
     <p>footer mm&copy;</p>
-  </div>
+    <div className='container row justify-content-center bg-dark py-4 rounded-5 border border-2 border-primary '>
+    <textarea name="" id="" cols="30" className='rounded-5 p-3 ' rows="10"></textarea>
+     <button className='col-2 rounded '>bg-color</button> 
+     
+     </div>
+     </div>
   )
 }
